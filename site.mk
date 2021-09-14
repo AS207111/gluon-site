35df12869fa5d23f686c5660f1f6ad28cd08093e
René Preuß <rene@preuss.io>
2021-09-14 21:30:01 +0200

    Update site.mk

--- site.mk
+++ site.mk
@@ -31,7 +31,7 @@ GLUON_SITE_PACKAGES := iwinfo
 #			opkg compare-versions "$1" '>>' "$2"
 #		to decide if a version is newer or not.
 
-DEFAULT_GLUON_RELEASE := 0.6+exp$(shell date '+%Y%m%d')
+DEFAULT_GLUON_RELEASE := v2021.1+exp$(shell date '+%Y%m%d')
 
 # Variables set with ?= can be overwritten from the command line
 
